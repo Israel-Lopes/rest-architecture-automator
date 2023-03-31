40eb531d8fc32cd94896e92050244462888ef485
Israel-Lopes <yisrael.lopes@gmail.com>
2023-03-31 15:23:57 -0300

    correcao token e cors

--- modules/create_classes.sh
+++ modules/create_classes.sh
@@ -10,20 +10,23 @@ for CLASS in "${DOMAIN_CLASSES[@]}"; do
   # Criar arquivos da classe na pasta correspondente
   touch "$MAIN_PATH/persistence/entity/${CLASS_NAME}Entity.java"
   touch "$MAIN_PATH/repository/${CLASS_NAME}Repository.java"
-  touch "$MAIN_PATH/service/cors/${CLASS_NAME}Cors.java"
   touch "$MAIN_PATH/service/model/${CLASS_NAME}Model.java"
-  touch "$MAIN_PATH/service/token/${CLASS_NAME}Token.java"
   touch "$MAIN_PATH/web/controller/${CLASS_NAME}Controller.java"
   touch "$MAIN_PATH/web/mapper/${CLASS_NAME}Mapper.java"
 
   touch "$TEST_PATH/persistence/entity/${CLASS_NAME}EntityTest.java"
   touch "$TEST_PATH/repository/${CLASS_NAME}RepositoryTest.java"
-  touch "$TEST_PATH/service/cors/${CLASS_NAME}CorsTest.java"
   touch "$TEST_PATH/service/model/${CLASS_NAME}ModelTest.java"
-  touch "$TEST_PATH/service/token/${CLASS_NAME}TokenTest.java"
   touch "$TEST_PATH/web/controller/${CLASS_NAME}ControllerTest.java"
   touch "$TEST_PATH/web/mapper/${CLASS_NAME}MapperTest.java"
 
 done
+
+touch "$MAIN_PATH/service/token/Token.java"
+touch "$MAIN_PATH/service/cors/Cors.java"
+
+touch "$TEST_PATH/service/cors/CorsTest.java"
+touch "$TEST_PATH/service/token/TokenTest.java"
+
 
 echo "Arquivos criados com sucesso!"
